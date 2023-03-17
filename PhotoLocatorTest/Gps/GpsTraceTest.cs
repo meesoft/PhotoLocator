60280373e44ffe916268054d07f78e1609e07ef9
Michael Vinther <mv@logicnet.dk>
2023-03-17 20:04:54 +0100

    HTML to GPX test

--- PhotoLocatorTest/Gps/GpsTraceTest.cs
+++ PhotoLocatorTest/Gps/GpsTraceTest.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Xml;
+
 namespace PhotoLocator.Gps
 {
     [TestClass]
@@ -34,6 +37,54 @@ namespace PhotoLocator.Gps
             var trace = KmlDecoder.DecodeStream(stream, TimeSpan.FromMinutes(15)).Single();
 
             Assert.AreEqual(259, trace.Locations.Count);
+        }
+
+        [TestMethod, Ignore]
+        public void ConvertHtmlTableToGpx()
+        {
+            const string Path = @"C:\temp\";
+
+            var src = new XmlDocument();
+            src.Load(Path + "Fredede fortidsminder i Slagelse Kommune.xml");
+            var root = src["doc"] ?? throw new FileFormatException("doc node missing");
+
+            var settings = new XmlWriterSettings();
+            settings.Indent = true;
+            using var dst = XmlWriter.Create(Path + "Fredede fortidsminder i Slagelse Kommune.gpx", settings);
+            dst.WriteStartDocument();
+            dst.WriteStartElement(null, "gpx", "http://www.topografix.com/GPX/1/1");
+            foreach (var row in root.OfType<XmlNode>().Where(n => n.Name == "tr"))
+            {
+                var titleCol = row.ChildNodes[1] ?? throw new FileFormatException("title col missing");
+                var refCol = row.ChildNodes[4] ?? throw new FileFormatException("ref col missing");
+                var gpsCol = row.ChildNodes[5] ?? throw new FileFormatException("gps col missing");
+                
+                var title = titleCol["a"]?.InnerText + " " + refCol["a"]?.InnerText;
+
+                var gpsText = gpsCol["span"]?["a"]?.InnerText ?? throw new FileFormatException("gps coordinates missing");
+                var gpsParts = gpsText.Split("/")[2].Split(";", StringSplitOptions.TrimEntries);
+                var lat = gpsParts[0];
+                var lon = gpsParts[1].Split(" ", StringSplitOptions.TrimEntries)[0].TrimEnd((char)65279);
+
+                dst.WriteStartElement("trk");
+                {
+                    dst.WriteElementString("name", title);
+                    dst.WriteStartElement("trkseg");
+                    {
+                        dst.WriteStartElement("trkpt");
+                        {
+                            dst.WriteAttributeString("lat", lat);
+                            dst.WriteAttributeString("lon", lon);
+                            dst.WriteElementString("time", "2000-01-01");
+                        }
+                        dst.WriteEndElement();
+                    }
+                    dst.WriteEndElement();
+                }
+                dst.WriteEndElement();
+            }
+            dst.WriteEndElement();
+            dst.WriteEndDocument();
         }
     }
 }
