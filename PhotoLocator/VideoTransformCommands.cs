c8361144b4fec09a1d10a1be3c48594a58883fa8
Michael Vinther <mv@logicnet.dk>
2024-05-23 22:53:29 +0200

    More transformations

--- PhotoLocator/VideoTransformCommands.cs
+++ PhotoLocator/VideoTransformCommands.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
 using PhotoLocator.Helpers;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -21,14 +23,14 @@ namespace PhotoLocator
             if (_mainViewModel.SelectedPicture is null)
                 return;
             var inPath = _mainViewModel.SelectedPicture.FullPath;
-            var outPath = Path.Combine(Path.GetDirectoryName(inPath)!, Path.GetFileNameWithoutExtension(inPath), "Frame%04d.jpg");
+            var outPath = Path.Combine(Path.GetDirectoryName(inPath)!, Path.GetFileNameWithoutExtension(inPath), "%06d.jpg");
             outPath = Interaction.InputBox($"Output path:", "Extract frames", outPath);
             if (string.IsNullOrEmpty(outPath))
                 return;
-            Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
             await _mainViewModel.RunProcessWithProgressBarAsync(async progressCallback =>
             {
                 progressCallback(-1);
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
                 await ProcessFileAsync($"-i \"{inPath}\" \"{outPath}\"");
             }, "Processing");
         });
@@ -44,14 +46,21 @@ namespace PhotoLocator
                 return;
 
             var inPath = _mainViewModel.SelectedPicture.FullPath;
-            var outPath = Path.Combine(Path.GetDirectoryName(inPath)!, Path.GetFileNameWithoutExtension(inPath) + "[stab]" + Path.GetExtension(inPath));
-            outPath = Interaction.InputBox($"Output path:", "Stabilize", outPath);
-            if (string.IsNullOrEmpty(outPath))
+            var dlg = new SaveFileDialog();
+            dlg.Title = "Stabilize";
+            dlg.InitialDirectory = Path.GetDirectoryName(inPath);
+            dlg.FileName = Path.Combine(dlg.InitialDirectory!, Path.GetFileNameWithoutExtension(inPath) + "[stabilized]" + Path.GetExtension(inPath));
+            dlg.DefaultExt = ".mp4";
+            dlg.CheckPathExists = false;
+            if (dlg.ShowDialog() != true)
                 return;
-            Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
+            var outPath = dlg.FileName;
+
             await _mainViewModel.RunProcessWithProgressBarAsync(async progressCallback =>
             {
                 progressCallback(-1);
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
+                File.Delete(outPath);
                 Directory.SetCurrentDirectory(Path.GetDirectoryName(inPath)!);
                 await ProcessFileAsync($"-i \"{inPath}\" -vf vidstabdetect=shakiness=7 -f null -");
                 await ProcessFileAsync($"-i \"{inPath}\" {vfArgs} \"{outPath}\"");
@@ -59,10 +68,75 @@ namespace PhotoLocator
             }, "Processing");
         });
 
+        public ICommand StabilizeAndExtract => new RelayCommand(async o =>
+        {
+            if (_mainViewModel.SelectedPicture is null)
+                return;
+
+            var vfArgs = "-vf \"vidstabtransform=smoothing=0:zoom=0:input=transforms.trf\"";
+            vfArgs = Interaction.InputBox($"Video filter arguments:", "Stabilize and extract frames", vfArgs);
+            if (string.IsNullOrEmpty(vfArgs))
+                return;
+
+            var inPath = _mainViewModel.SelectedPicture.FullPath;
+            var outPath = Path.Combine(Path.GetDirectoryName(inPath)!, Path.GetFileNameWithoutExtension(inPath) + "[stab]", "%06d.jpg");
+            outPath = Interaction.InputBox($"Output path:", "Stabilize and extract frames", outPath);
+            if (string.IsNullOrEmpty(outPath))
+                return;
+
+            await _mainViewModel.RunProcessWithProgressBarAsync(async progressCallback =>
+            {
+                progressCallback(-1);
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
+                File.Delete(outPath);
+                Directory.SetCurrentDirectory(Path.GetDirectoryName(inPath)!);
+                await ProcessFileAsync($"-i \"{inPath}\" -vf vidstabdetect=shakiness=7 -f null -");
+                await ProcessFileAsync($"-i \"{inPath}\" {vfArgs} \"{outPath}\"");
+                File.Delete("transforms.trf");
+            }, "Processing");
+        });
+
+        public ICommand Combine => new RelayCommand(async o =>
+        {
+            const string inputFileName = "input.txt";
+
+            var allSelected = _mainViewModel.GetSelectedItems().Where(item => item.IsFile).ToArray();
+            if (allSelected.Length == 0)
+                throw new UserMessageException("No images selected");
+
+            var args = "-c:v libx264 -r 30 -pix_fmt yuv420p";
+            args = Interaction.InputBox($"Video filter arguments:", $"Combine {allSelected.Length} images to video", args);
+            if (string.IsNullOrEmpty(args))
+                return;
+
+            var outPath = Path.GetDirectoryName(allSelected[0].FullPath);
+            var dlg = new SaveFileDialog();
+            dlg.Title = "Combine";
+            dlg.InitialDirectory = outPath;
+            dlg.FileName = outPath + '\\' + Path.GetFileNameWithoutExtension(outPath) + ".mp4";
+            dlg.DefaultExt = ".mp4";
+            dlg.CheckPathExists = false;
+            if (dlg.ShowDialog() != true)
+                return;
+            outPath = dlg.FileName;
+
+            await _mainViewModel.RunProcessWithProgressBarAsync(async progressCallback =>
+            {
+                progressCallback(-1);
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath)!);
+                File.Delete(outPath);
+                Directory.SetCurrentDirectory(Path.GetDirectoryName(allSelected[0].FullPath)!);
+                File.WriteAllLines(inputFileName, allSelected.Select(f => $"file '{f.Name}'"));
+                await ProcessFileAsync($"-f concat -safe 0 -i {inputFileName} {args} \"{outPath}\"");
+                File.Delete(inputFileName);
+            }, "Processing");
+        });
 
         private async Task ProcessFileAsync(string args, bool redirectStandardError = false)
         {
-            var startInfo = new ProcessStartInfo(_mainViewModel.Settings.FFmpegPath ?? throw new UserMessageException("FFmpeg path must be set in Settings"), args);
+            if (string.IsNullOrEmpty(_mainViewModel.Settings.FFmpegPath))
+                throw new UserMessageException("FFmpeg must be installed and the path configured in Settings");
+            var startInfo = new ProcessStartInfo(_mainViewModel.Settings.FFmpegPath, args);
             startInfo.RedirectStandardError = redirectStandardError;
             var process = Process.Start(startInfo) ?? throw new IOException("Failed to start FFmpeg");
             var output = redirectStandardError ? process.StandardError.ReadToEnd() : string.Empty; // We must read before waiting
