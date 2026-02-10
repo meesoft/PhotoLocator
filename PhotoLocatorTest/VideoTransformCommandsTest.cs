b53226168777bbf8acd7a43220212e456a01d8ff
Michael Vinther <mv@logicnet.dk>
2026-02-10 23:45:47 +0100

    Fix test

--- PhotoLocatorTest/VideoTransformCommandsTest.cs
+++ PhotoLocatorTest/VideoTransformCommandsTest.cs
@@ -1,37 +1,27 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using PhotoLocator.Helpers;
 using PhotoLocator.Settings;
-using System;
 using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace PhotoLocator
 {
     [TestClass]
     public class VideoTransformCommandsTest
     {
+        public TestContext TestContext { get; set; }
+
         [STATestMethod]
-        public async Task ProcessVideo_WithEachEffect()
+        public void ProcessSelected_ShouldHandleAllEffects()
         {
-            if (!File.Exists(VideoProcessingTest.FFmpegPath))
-                Assert.Inconclusive("FFmpegPath not found");
-            if (!File.Exists(VideoProcessingTest.SourceVideoPath))
-                Assert.Inconclusive("Source video not found");
-
-            Task executeTask = null;
-
             var testDir = Directory.GetCurrentDirectory();
-            var outputFileName = Path.Combine(testDir, "out.mp4");
+            var ffmpegPath = Path.Combine(testDir, VideoProcessingTest.FFmpegPath);
+            if (!File.Exists(ffmpegPath))
+                Assert.Inconclusive("FFmpegPath not found: " + ffmpegPath);
+            if (!File.Exists(VideoProcessingTest.SourceVideoPath))
+                Assert.Inconclusive("Source video not found: " + VideoProcessingTest.SourceVideoPath);
 
-            var settings = new ObservableSettings() { FFmpegPath = VideoProcessingTest.FFmpegPath };
+            Task? processTask = null;
+            var settings = new ObservableSettings() { FFmpegPath = ffmpegPath };
             var mainViewModelMoq = new Mock<IMainViewModel>();
             mainViewModelMoq.Setup(m => m.Settings).Returns(settings);
             mainViewModelMoq.Setup(m => m.GetSelectedItems(It.IsAny<bool>())).Returns(
@@ -40,23 +30,20 @@ namespace PhotoLocator
             mainViewModelMoq.Setup(m => m.RunProcessWithProgressBarAsync(It.IsAny<Func<Action<double>, CancellationToken, Task>>(), It.IsAny<string>(), It.IsAny<PictureItemViewModel>()))
                 .Returns((Func<Action<double>, CancellationToken, Task> body, string text, PictureItemViewModel focusItem) =>
                 {
-                    return executeTask = body(_ => { }, CancellationToken.None);
+                    return processTask = body(_ => { }, CancellationToken.None);
                 });
-            mainViewModelMoq.Setup(m => m.AddOrUpdateItemAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                .Returns((string path, bool isDir, bool select) => Task.CompletedTask);
 
+            var outputFileName = Path.Combine(testDir, "effect.mp4");
             foreach (var effectItem in VideoTransformCommands.Effects)
             {
                 File.Delete(outputFileName);
-
-                //Debug.WriteLine(effectItem.Content);
+                Debug.WriteLine("Using effect: " + effectItem.Content);
                 var commands = new VideoTransformCommands(mainViewModelMoq.Object);
                 commands.SelectedEffect = effectItem;
                 commands.ProcessSelected.Execute(outputFileName);
-                await (executeTask ?? throw new Exception("Task not started")).ConfigureAwait(true);
+                processTask?.Wait(TestContext.CancellationToken);
 
                 Assert.IsTrue(File.Exists(outputFileName));
-                File.Delete(outputFileName);
             }
         }
     }
