50d909ac003a55ab65bebfa56de19039cbf502ac
Michael Vinther <mv@logicnet.dk>
2026-02-22 16:08:12 +0100

    Use instance flags

--- PhotoLocator/Controls/DropDownSlider.xaml.cs
+++ PhotoLocator/Controls/DropDownSlider.xaml.cs
@@ -7,7 +7,7 @@ namespace PhotoLocator.Controls;
 
 public partial class DropDownSlider : UserControl
 {
-    static bool _onTextChangedRunning, _onNumericValueChangedRunning;
+    bool _onTextChangedRunning, _onNumericValueChangedRunning;
 
     public DropDownSlider()
     {
@@ -25,13 +25,13 @@ public partial class DropDownSlider : UserControl
 
     static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (_onNumericValueChangedRunning || d is not DropDownSlider control)
+        if (d is not DropDownSlider control || control._onNumericValueChangedRunning)
             return;
         if (double.TryParse(control.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out var v))
         {
-            _onTextChangedRunning = true;
+            control._onTextChangedRunning = true;
             control.NumericValue = Math.Clamp(v, control.Minimum, control.Maximum);
-            _onTextChangedRunning = false;
+            control._onTextChangedRunning = false;
         }
     }
 
@@ -43,42 +43,45 @@ public partial class DropDownSlider : UserControl
         nameof(Maximum), typeof(double), typeof(DropDownSlider), new PropertyMetadata(100.0));
     public double Maximum { get => (double)GetValue(MaximumProperty); set => SetValue(MaximumProperty, value); }
 
+    public static readonly DependencyProperty TickFrequencyProperty = DependencyProperty.Register(
+        nameof(TickFrequency), typeof(double), typeof(DropDownSlider), new PropertyMetadata(1.0));
+    public double TickFrequency { get => (double)GetValue(TickFrequencyProperty); set => SetValue(TickFrequencyProperty, value); }
+
     public static readonly DependencyProperty NumericValueProperty = DependencyProperty.Register(
         nameof(NumericValue), typeof(double), typeof(DropDownSlider), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnNumericValueChanged));
     public double NumericValue { get => (double)GetValue(NumericValueProperty); set => SetValue(NumericValueProperty, value); }
 
     public static readonly DependencyProperty DecimalsProperty = DependencyProperty.Register(
-        nameof(Decimals), typeof(int), typeof(DropDownSlider), new PropertyMetadata(1, OnDecimalsChanged));
+        nameof(Decimals), typeof(int), typeof(DropDownSlider), new PropertyMetadata(1, OnFormatChanged));
     public int Decimals { get => (int)GetValue(DecimalsProperty); set => SetValue(DecimalsProperty, value); }
 
-    static void OnDecimalsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    public static readonly DependencyProperty SliderValueUnitsProperty = DependencyProperty.Register(
+        nameof(SliderValueUnits), typeof(string), typeof(DropDownSlider), new PropertyMetadata(string.Empty, OnFormatChanged));
+    public string SliderValueUnits { get => (string)GetValue(SliderValueUnitsProperty); set => SetValue(SliderValueUnitsProperty, value); }
+
+    static void OnFormatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is DropDownSlider control)
             control.UpdateFormattedValue();
     }
 
-    public static readonly DependencyProperty TickFrequencyProperty = DependencyProperty.Register(
-        nameof(TickFrequency), typeof(double), typeof(DropDownSlider), new PropertyMetadata(1.0));
-    public double TickFrequency { get => (double)GetValue(TickFrequencyProperty); set => SetValue(TickFrequencyProperty, value); }
-
     public static readonly DependencyProperty FormattedNumericValueProperty = DependencyProperty.Register(
         nameof(FormattedNumericValue), typeof(string), typeof(DropDownSlider), new PropertyMetadata(string.Empty));
-    public string FormattedNumericValue { get => (string)GetValue(FormattedNumericValueProperty); set => SetValue(FormattedNumericValueProperty, value); }
+    public string FormattedNumericValue { get => (string)GetValue(FormattedNumericValueProperty); private set => SetValue(FormattedNumericValueProperty, value); }
 
     static void OnNumericValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is not DropDownSlider control)
             return;
-        _onNumericValueChangedRunning = true;
-        if (!_onTextChangedRunning)
+        control._onNumericValueChangedRunning = true;
+        if (!control._onTextChangedRunning)
             control.Text = control.NumericValue.ToString("F" + control.Decimals.ToString(CultureInfo.InvariantCulture), CultureInfo.InvariantCulture);
         control.UpdateFormattedValue();
-        _onNumericValueChangedRunning = false;
+        control._onNumericValueChangedRunning = false;
     }
 
     void UpdateFormattedValue()
     {
-        var fmt = "F" + Decimals.ToString(CultureInfo.InvariantCulture);
-        FormattedNumericValue = NumericValue.ToString(fmt, CultureInfo.InvariantCulture);
+        FormattedNumericValue = NumericValue.ToString("F" + Decimals.ToString(CultureInfo.InvariantCulture), CultureInfo.CurrentCulture) + SliderValueUnits;
     }
 }
