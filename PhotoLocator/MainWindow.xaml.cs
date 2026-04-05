ad8641a4ead3270afd8ad4723087a1bc4597b837
Michael Vinther <mv@logicnet.dk>
2026-04-05 08:57:20 +0200

    Change crop control background to pattern (#77)

--- PhotoLocator/MainWindow.xaml.cs
+++ PhotoLocator/MainWindow.xaml.cs
@@ -405,8 +405,9 @@ namespace PhotoLocator
                 _viewModel.IsCropControlVisible = false;
             else
             {
+                using var cursor = new MouseCursorOverride();
                 var imageScale = Math.Min(PreviewCanvas.ActualWidth / sourceImage.PixelWidth, PreviewCanvas.ActualHeight / sourceImage.PixelHeight);
-                CropGrid.Reset(sourceImage.PixelWidth, sourceImage.PixelHeight, imageScale, _viewModel.Settings.CropWidthHeightRatio);
+                CropGrid.Reset(sourceImage, imageScale, _viewModel.Settings.CropWidthHeightRatio);
                 _viewModel.CropControl = CropGrid;
             }
         }
