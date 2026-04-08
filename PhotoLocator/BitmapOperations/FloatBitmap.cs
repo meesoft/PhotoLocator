33997d4913717dd5aa38880324a54c8c40c41c0a
Michael Vinther <mv@logicnet.dk>
2026-04-08 22:47:55 +0200

    Revert unrolling

--- PhotoLocator/BitmapOperations/FloatBitmap.cs
+++ PhotoLocator/BitmapOperations/FloatBitmap.cs
@@ -109,7 +109,8 @@ namespace PhotoLocator.BitmapOperations
                         fixed (float* dstRow = &Elements[y, 0])
                         fixed (byte* srcRow = &sourcePixels[y * Stride])
                         {
-                            for (var x = 0; x < Width; x++)
+                            var width = Width;
+                            for (var x = 0; x < width; x++)
                             {
                                 dstRow[x * 3 + 2] = gamma[srcRow[x * 3 + 0]];
                                 dstRow[x * 3 + 1] = gamma[srcRow[x * 3 + 1]];
@@ -136,46 +137,12 @@ namespace PhotoLocator.BitmapOperations
                         fixed (float* dstRow = &Elements[y, 0])
                         fixed (byte* srcRow = &sourcePixels[y * Width * 4])
                         {
-                            var w = Width;
-                            var srcIndex = 0;
-                            var dstIndex = 0;
-                            int x = 0;
-
-                            // Process 4 pixels per iteration to reduce loop overhead
-                            for (; x + 3 < w; x += 4)
+                            var width = Width;
+                            for (var x = 0; x < width; x++)
                             {
-                                // pixel 0
-                                dstRow[dstIndex + 2] = gamma[srcRow[srcIndex + 0]];
-                                dstRow[dstIndex + 1] = gamma[srcRow[srcIndex + 1]];
-                                dstRow[dstIndex + 0] = gamma[srcRow[srcIndex + 2]];
-
-                                // pixel 1
-                                dstRow[dstIndex + 5] = gamma[srcRow[srcIndex + 4 + 0]];
-                                dstRow[dstIndex + 4] = gamma[srcRow[srcIndex + 4 + 1]];
-                                dstRow[dstIndex + 3] = gamma[srcRow[srcIndex + 4 + 2]];
-
-                                // pixel 2
-                                dstRow[dstIndex + 8] = gamma[srcRow[srcIndex + 8 + 0]];
-                                dstRow[dstIndex + 7] = gamma[srcRow[srcIndex + 8 + 1]];
-                                dstRow[dstIndex + 6] = gamma[srcRow[srcIndex + 8 + 2]];
-
-                                // pixel 3
-                                dstRow[dstIndex + 11] = gamma[srcRow[srcIndex + 12 + 0]];
-                                dstRow[dstIndex + 10] = gamma[srcRow[srcIndex + 12 + 1]];
-                                dstRow[dstIndex + 9] = gamma[srcRow[srcIndex + 12 + 2]];
-
-                                srcIndex += 16;
-                                dstIndex += 12;
-                            }
-
-                            // Remainder
-                            for (; x < w; x++)
-                            {
-                                dstRow[dstIndex + 2] = gamma[srcRow[srcIndex + 0]];
-                                dstRow[dstIndex + 1] = gamma[srcRow[srcIndex + 1]];
-                                dstRow[dstIndex + 0] = gamma[srcRow[srcIndex + 2]];
-                                srcIndex += 4;
-                                dstIndex += 3;
+                                dstRow[x * 3 + 2] = gamma[srcRow[x * 4 + 0]];
+                                dstRow[x * 3 + 1] = gamma[srcRow[x * 4 + 1]];
+                                dstRow[x * 3 + 0] = gamma[srcRow[x * 4 + 2]];
                             }
                         }
                     });
@@ -227,7 +194,8 @@ namespace PhotoLocator.BitmapOperations
                     fixed (Int16* srcPix = &src.Elements[y, 0])
                     fixed (float* dstPix = &Elements[y, 0])
                     {
-                        for (int x = 0; x < Width; x++)
+                        var width = Width;
+                        for (int x = 0; x < width; x++)
                             dstPix[x] = remap(srcPix[x]);
                     }
                 });
