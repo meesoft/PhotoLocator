3ad7470c9b708615c56f8d5f4dd9bbe29ccfc2e4
Michael Vinther <mv@logicnet.dk>
2026-04-10 22:42:35 +0200

    Max op test

--- PhotoLocator/BitmapOperations/MaxFramesOperation.cs
+++ PhotoLocator/BitmapOperations/MaxFramesOperation.cs
@@ -15,6 +15,11 @@ namespace PhotoLocator.BitmapOperations
         public override void ProcessImage(BitmapSource image)
         {
             var pixels = PrepareFrame(image);
+            if (ProcessedImages == 1)
+            {
+                Parallel.For(0, pixels.Length, i => _accumulatorPixels[i] = pixels[i]);
+                return;
+            }
             Parallel.For(0, pixels.Length, i =>
             {
                 if (pixels[i] > _accumulatorPixels![i])
