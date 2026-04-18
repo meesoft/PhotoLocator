4ffb3aa4ea2e662351d550b197f328826d635842
Michael Vinther <mv@logicnet.dk>
2026-04-18 23:05:51 +0200

    Optimize local contrast

--- PhotoLocator/BitmapOperations/IIRSmoothOperation.cs
+++ PhotoLocator/BitmapOperations/IIRSmoothOperation.cs
@@ -37,7 +37,54 @@ namespace PhotoLocator.BitmapOperations
                     }
                 });
                 // Vertical smooth
-                Parallel.For(0, plane.Stride, x =>
+                int quaterWidth = plane.Stride / 4;
+                Parallel.For(0, quaterWidth, x4 =>
+                {
+                    int x = x4 * 4;
+                    var width = plane.Stride;
+                    fixed (float* pixels = plane.Elements)
+                    {
+                        var pix0 = &pixels[x];
+                        var value0 = *pix0;
+                        var pix1 = pix0 + 1;
+                        var value1 = *pix1;
+                        var pix2 = pix0 + 2;
+                        var value2 = *pix2;
+                        var pix3 = pix0 + 3;
+                        var value3 = *pix3;
+                        for (var y = plane.Height; y > 1; y--)
+                        {
+                            pix0 += width;
+                            value0 = (value0 * filterSize + *pix0) * scale;
+                            *pix0 = value0;
+                            pix1 += width;
+                            value1 = (value1 * filterSize + *pix1) * scale;
+                            *pix1 = value1;
+                            pix2 += width;
+                            value2 = (value2 * filterSize + *pix2) * scale;
+                            *pix2 = value2;
+                            pix3 += width;
+                            value3 = (value3 * filterSize + *pix3) * scale;
+                            *pix3 = value3;
+                        }
+                        for (var y = plane.Height; y > 1; y--)
+                        {
+                            pix0 -= width;
+                            value0 = (value0 * filterSize + *pix0) * scale;
+                            *pix0 = value0;
+                            pix1 -= width;
+                            value1 = (value1 * filterSize + *pix1) * scale;
+                            *pix1 = value1;
+                            pix2 -= width;
+                            value2 = (value2 * filterSize + *pix2) * scale;
+                            *pix2 = value2;
+                            pix3 -= width;
+                            value3 = (value3 * filterSize + *pix3) * scale;
+                            *pix3 = value3;
+                        }
+                    }
+                });
+                Parallel.For(quaterWidth * 4, plane.Stride, x =>
                 {
                     var width = plane.Stride;
                     fixed (float* pixels = plane.Elements)
